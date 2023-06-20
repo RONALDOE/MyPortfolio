68f17876b2ea1a111613dd9e3f800a2815ff25c0
RONALDOE <est.ronaldoernesto@gmail.com>
2023-06-19 21:00:52 -0400

    ola

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './modules/Navbar';
 import Hero from './modules/Hero';
-import AboutMe from './modules/AboutMe.jsx';
+import AboutMe from './modules/AboutMe';
+import Proyects from './modules/Proyects'
 const App = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
     <AboutMe/>
+    <Proyects/>
     </>
   );
 };
