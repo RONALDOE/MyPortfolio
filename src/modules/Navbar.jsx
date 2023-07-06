cd915ef86904ec9c15cd9442d68564b499d619b3
RONALDOE <est.ronaldoernesto@gmail.com>
2023-07-05 23:55:25 -0400

    asd

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
                         <li><a href="">INICIO</a></li>
                         <li><a href="/#aboutme">SOBRE MI</a></li>
                         {/* <li><a href="">EXPERIENCIA</a></li> */}
-                        <li><a href="">PROYECTOS</a></li>
+                        <li><a href="/#proyects">PROYECTOS</a></li>
                         <li><a href="">SERVICIOS</a></li>
                         <li><a href="">CONTACTO</a></li>
                     </ul>
--- src/modules/Proyects.jsx
+++ src/modules/Proyects.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import Tempus from '../assets/Tempuslogo.png'
 export default function Proyects() {
+
+
   return (
-    <div className="bg-black  h-[100vh]" >
-        <h1 className='text-[5rem] ml-4 text-white font-bebasn text-center '>PROYECTOS</h1>
-        (
-        )
+    <div className="bg-black  h-[100vh]" id='proyects'>
+        <h1 className='text-[5rem]   text-white font-bebasn text-center '>PROYECTOS</h1>
+        <div className=' mt-10 grow flex flex-col gap-12 p-12'>
+
+
+          <div className='shadow-2xl shadow-slate-900 w-2/5  h-grow  justify-center items-center flex flex-row bg-gray-800 bg-opacity-80 backdrop-blur-lg rounded drop-shadow-lg font-mono text-white text-xl text-justify p-4 gap-4'>
+
+    <div>
+
+          <h1 className='text-[2.5rem] text-center'>TEMPUS</h1>
+          <br/>
+          <p className='text-[1rem] text-justify '>Administrador de archivos en la nube de Amazon AW3 con sistema de login y manejo de grupos de usuarios. </p>
+    </div>
+          <img className='object-fit h-[12rem] rounded-md' src={Tempus} alt="" srcset="" />
+
+          </div>
+        </div>
 
     </div>
   )
